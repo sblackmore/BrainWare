30a21bbaca0dcfa264176587496089118c5d73eb
sblackmore@CORP.AMAZONWORKSPACES.COM <sblackmore@CORP.AMAZONWORKSPACES.COM>
2018-06-27 19:49:07 -0400

    Added stored procedures to run queries

---
+++ ProjectDB/Stored Procedures/GetOrderProducts.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetOrderProducts]
+AS
+BEGIN
+	SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price 
+	FROM orderproduct op 
+	INNER JOIN product p 
+	ON op.product_id=p.product_id
+END
---
+++ ProjectDB/Stored Procedures/GetOrdersForCompany.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetOrdersForCompany]
+AS
+BEGIN
+	SELECT c.name, o.description, o.order_id 
+	FROM company c 
+	INNER JOIN [order] o 
+	ON c.company_id=o.company_id
+END
